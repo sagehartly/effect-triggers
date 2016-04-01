3d148fca1750ce7d023637a14b6a62e5fff9207b
sagehartly <sage.hartly@gmail.com>
2016-04-01 17:15:25 -0400

    Began first effect

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.btn-show-hide').on('click', function () {
+  $('.box').toggleClass('is-shown');
+});
+
+$('.btn-move').on('click', function () {
+  $('.diamond').toggleClass('moving');
+});
+
+$('.btn-collapse-expand').on('click', function () {
+  $('.panel').toggleClass('collapse');
+});
